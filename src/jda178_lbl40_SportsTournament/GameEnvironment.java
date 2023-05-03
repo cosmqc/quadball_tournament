6ffe3bfeb997761dadd3a8863e596a42a3b400ff
Jake Dalton <jda178@cs18223kq.canterbury.ac.nz>
2023-05-04 11:07:18 +1200

    add partial game loop broken

--- src/jda178_lbl40_SportsTournament/GameEnvironment.java
+++ src/jda178_lbl40_SportsTournament/GameEnvironment.java
@@ -1,8 +1,32 @@
 package jda178_lbl40_SportsTournament;
 
+import java.util.Hashtable;
+import java.util.Scanner;
+
 public class GameEnvironment {
 	
-	public static void main(String[] args) {
-		System.out.println("3 weeks left :))");
+	Hashtable<String, Runnable> options;
+	
+	private void setupOptions() {
+		options.put("quit", () -> System.exit(0));
 	}
+
+	public static void main(String[] args) {
+		
+		GameEnvironment game = new GameEnvironment();
+		
+		game.options = new Hashtable<String, Runnable>();
+		game.setupOptions();
+		
+		Scanner inputScanner = new Scanner(System.in);
+		System.out.println("Welcome to QUADBALL!!!");
+		System.out.println("1, 2, 3, or quit");
+		String userInput = inputScanner.nextLine();
+		while (userInput != null) {
+			// output hashtable keys as options
+			// get whatever option is wanted, and .run() it
+		}
+		inputScanner.close();
+	}
+
 }
