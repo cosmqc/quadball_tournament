262958f1e05b22f7c1f88be39e1be761b9657c82
leobl <leobl@LAPTOP-JS500P7F.hub>
2023-05-19 19:33:49 +1200

    added sub in/out buttons

--- src/gui/ClubWindow.java
+++ src/gui/ClubWindow.java
@@ -70,6 +70,9 @@ public class ClubWindow {
 		tabbedPane.setBounds(10, 10, 866, 593);
 		frame.getContentPane().add(tabbedPane);
 		
+		DefaultListModel<Athlete> athleteTeamModel = new DefaultListModel<>();
+		athleteTeamModel.addAll(game.athletesInTeam);
+		
 		JPanel viewPanel = new JPanel();
 		tabbedPane.addTab("View Team", null, viewPanel, null);
 		viewPanel.setLayout(null);
@@ -77,29 +80,26 @@ public class ClubWindow {
 		JLabel lblAthletesInTeam = new JLabel("Athletes In Team");
 		lblAthletesInTeam.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAthletesInTeam.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblAthletesInTeam.setBounds(10, 12, 357, 44);
+		lblAthletesInTeam.setBounds(10, 12, 300, 44);
 		viewPanel.add(lblAthletesInTeam);
-		
-		DefaultListModel<Athlete> athleteTeamModel = new DefaultListModel<>();
-		athleteTeamModel.addAll(game.athletesInTeam);
 		JList<Athlete> athleteTeamList = new JList<Athlete>(athleteTeamModel);
-		athleteTeamList.setBounds(20, 53, 357, 250);
+		athleteTeamList.setBounds(20, 53, 300, 250);
 		viewPanel.add(athleteTeamList);
 		
 		JLabel lblAthletesInReserve = new JLabel("Athletes In Reserve");
 		lblAthletesInReserve.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAthletesInReserve.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblAthletesInReserve.setBounds(10, 313, 357, 44);
+		lblAthletesInReserve.setBounds(10, 313, 310, 44);
 		viewPanel.add(lblAthletesInReserve);
 		
 		JList<Athlete> athleteReserveList = new JList<Athlete>();
-		athleteReserveList.setBounds(20, 356, 357, 150);
+		athleteReserveList.setBounds(20, 356, 300, 150);
 		viewPanel.add(athleteReserveList);
 		
 		JTextPane txtpnInfo = new JTextPane();
 		txtpnInfo.setEditable(false);
 		txtpnInfo.setFont(new Font("Dialog", Font.PLAIN, 18));
-		txtpnInfo.setBounds(494, 53, 357, 250);
+		txtpnInfo.setBounds(550, 53, 300, 250);
 		viewPanel.add(txtpnInfo);
 		txtpnInfo.setText("No Athlete Selected");
 		athleteTeamList.addListSelectionListener(new ListSelectionListener() {
@@ -107,8 +107,8 @@ public class ClubWindow {
             public void valueChanged(ListSelectionEvent selectedItem) {
                 if (!selectedItem.getValueIsAdjusting()) {
                 	Athlete athleteInfo = athleteTeamList.getSelectedValue();
-                	txtpnInfo.setText(String.format("Name: %s \nOffence: %s \nDefence: %s \nSpeed: %s \nStamina: %s/%s"
-                			,athleteInfo.getName(),athleteInfo.getOffence(),athleteInfo.getDefence(),athleteInfo.getSpeed(),athleteInfo.getStamina(),athleteInfo.getRawStamina()));
+                	txtpnInfo.setText(String.format("Name: %s \nOffence: %s \nDefence: %s \nSpeed: %s \nStamina: %s/%s",
+                			athleteInfo.getName(),athleteInfo.getOffence(),athleteInfo.getDefence(),athleteInfo.getSpeed(),athleteInfo.getStamina(),athleteInfo.getRawStamina()));
                 }
             }
         });
@@ -119,6 +119,34 @@ public class ClubWindow {
 		lblAthleteInfo.setFont(new Font("Dialog", Font.BOLD, 18));
 		lblAthleteInfo.setBounds(494, 12, 357, 44);
 		viewPanel.add(lblAthleteInfo);
+		
+		JButton btnSubOut = new JButton("Sub Out");
+		btnSubOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (athleteTeamList.getSelectedValue() == null) {
+					JOptionPane.showMessageDialog(frame, "Please select an athlete to sub out", "No Athlete Selected",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					//TODO: Sub out athlete. Is this a Team.java?
+				}
+			}
+		});
+		btnSubOut.setBounds(330, 258, 97, 44);
+		viewPanel.add(btnSubOut);
+		
+		JButton btnSubIn = new JButton("Sub In");
+		btnSubIn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (athleteReserveList.getSelectedValue() == null) {
+					JOptionPane.showMessageDialog(frame, "Please select an athlete to sub in", "No Athlete Selected",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					//TODO: Sub in athlete. Is this a Team.java?
+				}
+			}
+		});
+		btnSubIn.setBounds(330, 462, 97, 44);
+		viewPanel.add(btnSubIn);
 		
 		JPanel inventoryPanel = new JPanel();
 		tabbedPane.addTab("Inventory", null, inventoryPanel, null);
